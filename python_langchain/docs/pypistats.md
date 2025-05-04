412e2368a2af09a8949c3d4ac9e174074174800e
Chris Joakim <christopher.joakim@gmail.com>
2025-05-04 10:43:09 -0400

    exploring pypistats, etc

--- python_langchain/docs/pypistats.md
+++ python_langchain/docs/pypistats.md
@@ -11,3 +11,23 @@
 > https://pypistats.org/
 
 Also see  Google BigQuery pypi downloads tables.
+
+```
+$ pypistats recent langchain -f json | jq
+{
+  "data": {
+    "last_day": 2195328,
+    "last_month": 72132900,
+    "last_week": 17271934
+  },
+  "package": "langchain",
+  "type": "recent_downloads"
+}
+```
+
+## Other Solutions
+
+pipdeptree
+
+curl https://registry.npmjs.org/m26-js | jq
+
--- python_langchain/main.py
+++ python_langchain/main.py
@@ -3,7 +3,7 @@ Usage:
   python main.py <func>
   python main.py env
   python main.py duck1
-  python main.py boto
+  python main.py ppstats
 Options:
   -h --help     Show this screen.
   --version     Show version.
@@ -17,9 +17,10 @@ import time
 import os
 import traceback
 
-import boto3
-
 import duckdb 
+import pypistats
+
+from pprint import pprint
 
 from docopt import docopt
 from dotenv import load_dotenv
@@ -68,65 +69,10 @@ def duck1():
     # title.principals.tsv.gz
     # title.ratings.tsv.gz
 
-def boto():
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-examples.html
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-creating-buckets.html
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
-    # https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-example-download-file.html
-
-    bucket_name = 'cjoakim-test1'
-
-    # Create a boto3 client
-    s3 = boto3.client('s3')
-
-    response = s3.list_buckets()
-
-    # Output the bucket names
-    print('List of Buckets:')
-    for bucket in response['Buckets']:
-        print("bucket: {}".format(bucket["Name"]))
-
-    print('Uploading:')
-    with open("data/postal_codes_nc.csv", "rb") as f:
-        s3.upload_fileobj(f, bucket_name, "postal_codes_nc.csv")
-
-    print('Downloading:')   
-    with open('tmp/postal_codes_nc.csv', 'wb') as f:
-        s3.download_fileobj(bucket_name, 'postal_codes_nc.csv', f)
-
-    response = s3.list_objects_v2(Bucket=bucket_name, MaxKeys=100)
-    print(response)
-    for c in response['Contents']:
-        key, size = c['Key'], c['Size']
-        print("key: {}  ({}) in {}".format(key, size, bucket_name))
-
-
-def boto_old():
-    # This seems to use the old 'boto3.resource' approach;
-    # prefer the newer 'boto3.client' approach in the above boto3() method.
-
-    bucket_name = 'cjoakim-test1'
-
-    s3 = boto3.resource('s3')
-
-    bucket = s3.Bucket(bucket_name)
-
-    for b in s3.buckets.all():
-        print("S3 bucket name: {}".format(b.name))
-
-    # Upload a new file
-    with open('requirements.txt', 'rb') as data:
-        bucket.put_object(Key='requirements2.txt', Body=data)
-
-    # List the contents of a bucket
-    for bucket_object in bucket.objects.all():
-        print("key: {} in bucket: {}".format(bucket_object.key, bucket_name))
-
-    #s3.download_file(bucket_name, 'requirements2.txt', 'requirements2_downloaded.txt')
-    client = boto3.client('s3')
-    with open('requirements2.txt', 'wb') as f:
-        client.download_fileobj(bucket_name, 'requirements2.txt', f)
         
+def ppstats():
+    print(pypistats.recent("m26"))
+
 
 if __name__ == "__main__":
     try:
@@ -135,8 +81,8 @@ if __name__ == "__main__":
             check_env()
         elif func == "duck1":
             duck1()
-        elif func == "boto":
-            boto()
+        elif func == "ppstats":
+            ppstats()
         else:
             print_options("Error: invalid function: {}".format(func))
     except Exception as e:
